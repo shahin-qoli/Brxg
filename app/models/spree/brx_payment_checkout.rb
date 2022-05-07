ca94a84657369903d7bd132678be200e8b3e5855
shahin <shaahin.qoli@gmail.com>
2022-05-07 12:57:30 -0700

    init

---
+++ app/models/spree/brx_payment_checkout.rb
@@ -0,0 +1,4 @@
+module Spree
+  class BrxPaymentCheckout < ActiveRecord::Base
+  end
+end
---
+++ app/models/spree/gateway/brx_gateway.rb
@@ -0,0 +1,19 @@
+
+module Spree
+  class Gateway::BrxGateway < Gateway
+    def provider_class
+      Spree::Gateway::BrxGateway
+    end
+    def payment_source_class
+      Spree::CreditCard
+    end
+
+    def method_type
+      'brx'
+    end
+
+    def purchase(amount, transaction_details, options = {})
+      ActiveMerchant::Billing::Response.new(true, 'success', {}, {})
+    end
+  end  
+end
