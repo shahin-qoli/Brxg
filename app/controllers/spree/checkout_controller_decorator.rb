0c046e42be0002e38259c92f30af931e1b2bc321
shahin <shaahin.qoli@gmail.com>
2022-05-08 06:11:34 -0700

    added Soltani to checkoutController

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -6,11 +6,11 @@ module Spree
         if @order.update_from_params(params, permitted_checkout_attributes, request.headers.env)
           payment = @order.payments.last
           payment.process!
-          mollie_payment_url = "https://burux.ir/"
-
+          #mollie_payment_url = "https://burux.ir/"
+          
           MollieLogger.debug("For order #{@order.number} redirect user to payment URL: #{mollie_payment_url}")
-
-          redirect_to mollie_payment_url
+          get_payment_url
+          redirect_to payment_url
         else
           render :edit
         end
@@ -18,7 +18,26 @@ module Spree
         super
       end
     end
+
+    def get_payment_url
+      amount = @order.amount 
+      options = { :body => 
+  {
+    "App":"SpreeApp",
+    "Type":"INV",
+    "Price":amount,
+    "Model":"{PaymentTitle:'پرداخت سفارش شماره تست بابت خرید از فروشگاه ویزیتور ها'}",
+    "CallbackAction":"RedirectToUrl",
+    "ForceRedirectBank":"true",
+    "CallbackUrl":"www.burux.ir"
+  }
+}
+                
+      results = HTTParty.post("https://shop.burux.com/api/PaymentService/Request", options)
+      payment_url = results[:InvoiceUrl]
+      RequestID = results[:RequestID]
   end
+
 
   module CheckoutControllerDecorator
     def payment_method_id_param
