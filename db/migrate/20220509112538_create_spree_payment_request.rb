d1d9792afcc665cc52acfb32dd8fa1b52cdd7dec
shahin <shaahin.qoli@gmail.com>
2022-05-10 03:33:16 -0700

    routes edited6

--- db/migrate/20220509112538_create_spree_payment_request.rb
+++ db/migrate/20220509112538_create_spree_payment_request.rb
@@ -2,7 +2,7 @@ require 'spree_extension/migration'
 
 class CreateSpreePaymentRequest < SpreeExtension::Migration[6.1]
   def change
-    create_table :spree_payment_request do |t|
+    create_table :spree_payment_requests do |t|
       t.belongs_to :order, index: { unique: true }, null: false
       t.string :requestid
       t.string :amount
