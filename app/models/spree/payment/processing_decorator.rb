1c60fe287dbb594ab4469ebfe27caa12be8ccad5
shahin <shaahin.qoli@gmail.com>
2022-05-09 22:55:42 -0700

    going tp final steps3

--- app/models/spree/payment/processing_decorator.rb
+++ app/models/spree/payment/processing_decorator.rb
@@ -1,14 +1,10 @@
 module Spree::Payment::ProcessingDecorator
 
   def capture!(amount = nil)
-    return true if completed?
-
-        started_processing!
-        money = ::Money.new(amount, currency)
-        capture_events.create!(amount: money.to_f)
-        split_uncaptured_amount
-
-    end
+      started_processing!
+      money = ::Money.new(amount, currency)
+      capture_events.create!(amount: money.to_f)
+      split_uncaptured_amount
   end
 end
-Spree::Payment.include(Spree::Payment::ProcessingDecorator)
+Spree::Payment::Processing.prepend(Spree::Payment::ProcessingDecorator)
