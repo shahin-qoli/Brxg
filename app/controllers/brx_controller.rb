aa57dd5465abb7a6f1a4eeed1f29958743c0d4a4
shahin <shaahin.qoli@gmail.com>
2022-05-10 01:33:11 -0700

    routes edited5

---
+++ app/controllers/brx_controller.rb
@@ -0,0 +1,35 @@
+module Spree
+  class BrxController < StoreController
+"""
+    def payment_method
+      Spree::PaymentMethod.find(params[:payment_method_id])
+    end
+
+    def provider
+      payment_method.provider
+    end
+
+
+    def confirm
+      order = current_order || raise(ActiveRecord::RecordNotFound)
+      order.payments.create!({
+        source: Spree::PaypalExpressCheckout.create({
+          token: params[:token],
+          payer_id: params[:PayerID]
+        }),
+        amount: order.total,
+        payment_method: payment_method
+      })
+      order.next
+      if order.complete?
+        flash.notice = Spree.t(:order_processed_successfully)
+        flash[:order_completed] = true
+        session[:order_id] = nil
+        redirect_to completion_route(order)
+      else
+        redirect_to checkout_state_path(order.state)
+      end
+    end    
+    """
+  end  
+end   
