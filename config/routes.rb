3230d440d9628be4d3981f4ad52a7738fd963078
shahin <shaahin.qoli@gmail.com>
2022-05-10 00:14:09 -0700

    routes edited3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Spree::Core::Engine.add_routes do
-   get '/bankpayment/:reqid/:payid/:type', :to => "CheckoutController#getandverify", :as => :get_verify
+   get '/bankpayment/:reqid/:payid/:type', :to => "checkoutcontroller#getandverify", :as => :get_verify
 end
 
