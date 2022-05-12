b465d1930d5c1b8ff06c2131d2d6e905223f8fe9
shahin <shaahin.qoli@gmail.com>
2022-05-12 02:16:57 -0700

    after meeting, redirecting and add payment66

--- app/models/spree/gateway/brx_gateway.rb
+++ app/models/spree/gateway/brx_gateway.rb
@@ -1,6 +1,6 @@
 
 module Spree
-  class Gateway::BrxGateway < PaymentMethod
+  class Gateway::BrxGateway < Gateway
     def provider_class
       Spree::Gateway::BrxGateway
     end
@@ -13,6 +13,15 @@ module Spree
       'brx'
     end
 
+    def supports?(source)
+      true
+    end
+    def provider
+      provider_class.new
+    end
+    def auto_capture?
+      true
+    end
     def purchase(amount, transaction_details, options = {})
       ActiveMerchant::Billing::Response.new(true, 'success', {}, {})
     end
