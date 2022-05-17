6a0e63e6de63f36b4089af5949039c51ece647cb
shahin <shaahin.qoli@gmail.com>
2022-05-16 23:27:51 -0700

    payment created by me brx controller/FALSE

--- app/controllers/spree/brx_controller.rb
+++ app/controllers/spree/brx_controller.rb
@@ -88,12 +88,14 @@ module Spree
                #order = current_order || raise(ActiveRecord::RecordNotFound)     
                @order_id_brx = @checkout_brx['order_id']
                @order_brx = Spree::Order.find(@order_id_brx)   
+               """
                @order_brx.payments.create!({
                 source: Spree::BrxExpressCheckout.create({
                   request_id: @request_id_brx,
                   amount: @amount_brx
                 }), amount: @amount_brx, payment_method: payment_method
-                })         
+                })
+               """          
                puts "the request id is HERE "
                puts "the request id is HERE "
                puts "the request id is HERE " 
