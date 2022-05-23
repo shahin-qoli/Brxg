5722b7cdf43827355053ab4ee6a4a78da2afaa84
shahin <shaahin.qoli@gmail.com>
2022-05-23 02:18:11 -0700

    postal code verify decorated

--- app/models/spree/address_decorator.rb
+++ app/models/spree/address_decorator.rb
@@ -2,7 +2,7 @@ module Spree::AddressDecorator
 	def postal_code_validate
 		return if country.blank? || country_iso.blank? || !require_zipcode? || zipcode.blank?
         return unless ::ValidatesZipcode::CldrRegexpCollection::ZIPCODES_REGEX.keys.include?(country_iso.upcase.to_sym)
-        errors.add(:zipcode, :invalid) unless zipcode.to_s.strip == 
+        errors.add(:zipcode, :invalid) unless zipcode.to_s.strip == 10
     end
 end
 
